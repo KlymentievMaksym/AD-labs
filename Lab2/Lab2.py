1423be02505ee906b3063d9818fb99c9301bd9b4
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-10-04 21:49:52 +0300

    Update Lab2.py
    Copied all code from Lab2_hvr

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,1 +1,49 @@
-pass
+# import matplotlib as mtplt
+# import numpy as np
+# import cherrypy
+# import jinja2
+# import dataspyre as 
+import urllib
+import json
+from spyre import server
+import pandas as pd
+
+class DropdownForVCI_TCI_VHI(server.App):
+    title = "NOAA data vizualization"
+    
+    inputs = [{"type": 'dropdown',
+               "label": 'NOAA data dropdown',
+               "options": [{"label":"VCI", "value":"VCI"},
+                          {"label":"TCI", "value":"TCI"},
+                          {"label":"VHI", "value":"VGI"}],
+               "key":'ticker',
+               "action_id": "update_data"}]
+    
+class TextEntryForMonthInterval(server.App):
+    title = "Text entry for month interval"
+    
+    inputs = [{"type": 'text',
+               "label": 'data-ranges',
+               "options": [{"label":"VCI", "value":"VCI"},
+                          {"label":"TCI", "value":"TCI"},
+                          {"label":"VHI", "value":"VGI"}],
+               "key":'range',
+               "value":'9-10',
+               "action_id": "simple_html_output"}]
+    
+    def getHTML(self, params):
+        ranges = params["range"]
+        return ranges
+
+class TableAndPlot(server.App):
+    title = "Table and plot"
+    
+    inputs = [{"type": 'plot',
+               "id": 'plot',
+               "control_id": 'update_data',
+               "tab": 'Plot'},
+               {"type": 'table',
+                "id": 'table_id',
+                "control_id": 'update_data',
+                "tab": 'Table',
+                "on_page_load": True}]
