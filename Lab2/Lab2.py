2d6343898c48d1ddf08c14267b9be5d2c85fa090
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-10-05 21:07:10 +0300

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -7,12 +7,8 @@
 # import json
 # from spyre import server
 # import pandas as pd
-# from ..__init__.py import *
-import os
-# print(os.getcwd())
-# input()
-# from .Lab1.Lab1 import receive_list_of_csv, get_dict_for_transfer
 
+# import os
 # class DropdownForVCI_TCI_VHI(server.App):
 #     title = "NOAA data vizualization"
     
@@ -52,20 +48,3 @@ import os
 #                 "control_id": 'update_data',
 #                 "tab": 'Table',
 #                 "on_page_load": True}]
-
-# list_of_csv = receive_list_of_csv()
-# print(list_of_csv)
-if not os.path.exists("Csv\\"):
-    create_VHI_dataset()
-    dict_for_df = get_dict_for_df()
-else:
-    k = 0
-    for name in os.listdir("Csv\\"):
-        path = "Csv\\" + name
-        df = read_VHI_dataframe(path)
-        dict_for_df[k] = df
-        k += 1
-dict_for_transfer = get_dict_for_transfer()
-
-# for file_name in list_of_csv:
-    # pass
