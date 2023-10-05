aeb31e7db284fa4629ac09b9fb3ac2a946af1f07
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-10-05 20:13:13 +0300

    Update Lab2.py

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -5,46 +5,56 @@
 # import dataspyre as 
 # import urllib
 # import json
-from spyre import server
+# from spyre import server
 # import pandas as pd
-# from ..Lab1.lab1 import create_VHI_dataset
+# from ..__init__.py import *
+# from ..Lab1.Lab1 import receive_list_of_csv, get_dict_for_transfer
 
-class DropdownForVCI_TCI_VHI(server.App):
-    title = "NOAA data vizualization"
+# class DropdownForVCI_TCI_VHI(server.App):
+#     title = "NOAA data vizualization"
     
-    inputs = [{"type": 'dropdown',
-               "label": 'NOAA data dropdown',
-               "options": [{"label":"VCI", "value":"VCI"},
-                          {"label":"TCI", "value":"TCI"},
-                          {"label":"VHI", "value":"VGI"}],
-               "key":'ticker',
-               "action_id": "update_data"}]
+#     inputs = [{"type": 'dropdown',
+#                "label": 'NOAA data dropdown',
+#                "options": [{"label":"VCI", "value":"VCI"},
+#                           {"label":"TCI", "value":"TCI"},
+#                           {"label":"VHI", "value":"VGI"}],
+#                "key":'ticker',
+#                "action_id": "update_data"}]
     
-class TextEntryForMonthInterval(server.App):
-    title = "Text entry for month interval"
+# class TextEntryForMonthInterval(server.App):
+#     title = "Text entry for month interval"
     
-    inputs = [{"type": 'text',
-               "label": 'data-ranges',
-               "options": [{"label":"VCI", "value":"VCI"},
-                          {"label":"TCI", "value":"TCI"},
-                          {"label":"VHI", "value":"VGI"}],
-               "key":'range',
-               "value":'9-10',
-               "action_id": "simple_html_output"}]
+#     inputs = [{"type": 'text',
+#                "label": 'data-ranges',
+#                "options": [{"label":"VCI", "value":"VCI"},
+#                           {"label":"TCI", "value":"TCI"},
+#                           {"label":"VHI", "value":"VGI"}],
+#                "key":'range',
+#                "value":'9-10',
+#                "action_id": "simple_html_output"}]
     
-    def getHTML(self, params):
-        ranges = params["range"]
-        return ranges
+#     def getHTML(self, params):
+#         ranges = params["range"]
+#         return ranges
 
-class TableAndPlot(server.App):
-    title = "Table and plot"
+# class TableAndPlot(server.App):
+#     title = "Table and plot"
     
-    inputs = [{"type": 'plot',
-               "id": 'plot',
-               "control_id": 'update_data',
-               "tab": 'Plot'},
-               {"type": 'table',
-                "id": 'table_id',
-                "control_id": 'update_data',
-                "tab": 'Table',
-                "on_page_load": True}]
+#     inputs = [{"type": 'plot',
+#                "id": 'plot',
+#                "control_id": 'update_data',
+#                "tab": 'Plot'},
+#                {"type": 'table',
+#                 "id": 'table_id',
+#                 "control_id": 'update_data',
+#                 "tab": 'Table',
+#                 "on_page_load": True}]
+
+list_of_csv = receive_list_of_csv()
+print(list_of_csv)
+dict_for_df = dict()
+dict_for_transfer = get_dict_for_transfer()
+input()
+
+for file_name in list_of_csv:
+    pass
