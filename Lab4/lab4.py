44faf347a1be4278870c9c1aaf481653189de964
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-11-26 16:46:24 +0200

    added phase

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -20,36 +20,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons, Button, Slider
 
-
-
-
 # The parametrized function to be plotted
-def f(t, amplitude, frequency):
-    return amplitude * np.sin(2 * np.pi * frequency * t)
+def f(t, amplitude, frequency, phase): # harmonic
+    return amplitude * np.sin(2 * np.pi * frequency * t + phase)
 
-def f1(t, amplitude, frequency): # harmonic_with_noise
-    return amplitude * np.sin(4 * np.pi * frequency * t)
+def f1(t, amplitude, frequency, phase): # harmonic_with_noise
+    return amplitude * np.sin(4 * np.pi * frequency * t + phase)
 
 t = np.linspace(0, 1, 1000)
 
 # Define initial parameters
 init_amplitude = 5
 init_frequency = 3
-
-
-# fig, ax = plt.subplots()
-# l0, = ax.plot(t, s0, visible=False, lw=2)
-# l1, = ax.plot(t, s1, lw=2)
-# l2, = ax.plot(t, s2, lw=2)
-# plt.subplots_adjust(left=0.2)
-
-# rax = plt.axes([0.05, 0.4, 0.1, 0.15])
-
+init_phase = 0
+init_noise_mean = 0
+init_noise_covariance = 0
 
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
-line, = ax.plot(t, f(t, init_amplitude, init_frequency), lw=2)
-line2, = ax.plot(t, f1(t, init_amplitude, init_frequency), lw=2)
+line, = ax.plot(t, f(t, init_amplitude, init_frequency, init_phase), lw=2)
+line2, = ax.plot(t, f1(t, init_amplitude, init_frequency, init_phase), visible=False, lw=2)
 ax.set_xlabel('Time [s]')
 
 # adjust the main plot to make room for the sliders
@@ -65,6 +55,16 @@ freq_slider = Slider(
     valinit=init_frequency,
 )
 
+# Make a horizontal slider to control the phase.
+axphas = fig.add_axes([0.25, 0.05, 0.65, 0.03])
+phas_slider = Slider(
+    ax=axphas,
+    label='Phase',
+    valmin=0,
+    valmax=2*np.pi,
+    valinit=init_phase,
+)
+
 # Make a vertically oriented slider to control the amplitude
 axamp = fig.add_axes([0.1, 0.25, 0.0225, 0.63])
 amp_slider = Slider(
@@ -76,37 +76,38 @@ amp_slider = Slider(
     orientation="vertical"
 )
 
+rax = fig.add_axes([0.40, 0.89, 0.35, 0.12])
+check = CheckButtons(rax, ('Harmonic', 'Harmonic via noise'), (True, False))
 
 # The function to be called anytime a slider's value changes
 def update(val):
-    line.set_ydata(f(t, amp_slider.val, freq_slider.val))
+    line.set_ydata(f(t, amp_slider.val, freq_slider.val, phas_slider.val))
+    line2.set_ydata(f1(t, amp_slider.val, freq_slider.val, phas_slider.val))
     fig.canvas.draw_idle()
 
 
-rax = fig.add_axes([0.05, 0.4, 0.1, 0.15])
-check = CheckButtons(rax, ('2 Hz', '4 Hz'), (False, True))
-
 def func(label):
-    if label == '2 Hz':
+    if label == 'Harmonic':
         line.set_visible(not line.get_visible())
-    elif label == '4 Hz':
+    elif label == 'Harmonic via noise':
         line2.set_visible(not line2.get_visible())
     plt.draw()
-
 
 # register the update function with each slider
 check.on_clicked(func)
 freq_slider.on_changed(update)
 amp_slider.on_changed(update)
+phas_slider.on_changed(update)
 
 # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+resetax = fig.add_axes([0.8, 0.005, 0.1, 0.04])
 button = Button(resetax, 'Reset', hovercolor='0.975')
 
 
 def reset(event):
     freq_slider.reset()
     amp_slider.reset()
+    phas_slider.reset()
 button.on_clicked(reset)
 
 plt.show()
