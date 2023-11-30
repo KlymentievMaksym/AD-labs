09043b71e9ce7985c9e0e34450508a1fff9c3a66
Dadmon <maksklym-ipt27@lll.kpi.ua>
2023-11-30 13:02:54 +0200

    Move to class and stable generator
    Relocated matplot lib on class and generator now changes only if changes some of noises parameters

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -39,169 +39,187 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import CheckButtons, Button, Slider
 from scipy import signal
 
-# The parametrized function to be plotted
-def f(t, amplitude, frequency, phase): # harmonic
-    return amplitude * np.sin(2 * np.pi * frequency * t + phase)
+class MPLTTask():
 
-def f1(t, amplitude, frequency, phase, noise_mean, noise_covariance): # harmonic_with_noise
-    noise = np.random.normal(noise_mean, noise_covariance, len(t))
-    return amplitude * np.sin(2 * np.pi * frequency * t + phase) + noise
-
-t = np.linspace(0, 1, 1000)
-
-# Define initial parameters
-init_amplitude = 5
-init_frequency = 3
-init_phase = 0
-init_noise_mean = 0
-init_noise_covariance = 0
-init_filter = 0.03
-
-# Create the figure and the line that we will manipulate
-fig, (ax0, ax1) = plt.subplots(1, 2)
-
-
-line2, = ax0.plot(t, f1(t, init_amplitude, init_frequency, init_phase, init_noise_mean, init_noise_covariance), visible=False, color='#FF5722', lw=2)
-line, = ax0.plot(t, f(t, init_amplitude, init_frequency, init_phase), color='#3F51B5', lw=2)
-
-# b, a = signal.iirfilter(3, 0.5, btype='low')
-b, a = signal.butter(4, init_filter, btype='low')#, analog=False)
-
-filtered_harmonic = signal.lfilter(b, a, f1(t, init_amplitude, init_frequency, init_phase, init_noise_mean, init_noise_covariance))
-line3, = ax1.plot(t, filtered_harmonic, visible=False, lw=2)
-
-ax0.set_xlabel('Time [s]')
-ax0.set_title('Harmonic')
-ax1.set_title('Filtered Harmonic')
-
-# adjust the main plot to make room for the sliders
-fig.subplots_adjust(left=0.25, bottom=0.35, top=0.75)
-
-# Make a horizontal oriented slider to control the amplitude
-axamp = fig.add_axes([0.25, 0.2, 0.65, 0.03])
-amp_slider = Slider(
-    ax=axamp,
-    label="Amplitude",
-    valmin=0,
-    valmax=10,
-    valinit=init_amplitude,
-    #orientation="vertical"
-    color='red'
-)
-
-# Make a horizontal slider to control the frequency.
-axfreq = fig.add_axes([0.25, 0.15, 0.65, 0.03])
-freq_slider = Slider(
-    ax=axfreq,
-    label='Frequency [Hz]',
-    valmin=0.1,
-    valmax=30,
-    valinit=init_frequency,
-    color='grey'
-)
-
-# Make a horizontal slider to control the phase.
-axphas = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-phas_slider = Slider(
-    ax=axphas,
-    label='Phase',
-    valmin=0,
-    valmax=2*np.pi,
-    valinit=init_phase,
-    color='purple'
-)
-
-# Make a vertically oriented slider to control the Noise mean
-nmamp = fig.add_axes([0.15, 0.35, 0.0225, 0.53])
-noismean_slider = Slider(
-    ax=nmamp,
-    label="Mean",
-    valmin=-5,
-    valmax=5,
-    valinit=init_noise_mean,
-    orientation="vertical",
-    color='#3F51B5'
-)
-
-# Make a vertically oriented slider to control the Noise covariance
-ncamp = fig.add_axes([0.08, 0.35, 0.0225, 0.53])
-noiscovar_slider = Slider(
-    ax=ncamp,
-    label="Covar",
-    valmin=0,
-    valmax=5,
-    valinit=init_noise_covariance,
-    orientation="vertical",
-    color='#FF5722'
-)
-
-fltamp = fig.add_axes([0.95, 0.35, 0.0225, 0.53])
-flt_slider = Slider(
-    ax=fltamp,
-    label="Filter",
-    valmin=0.001,
-    valmax=0.1,
-    valinit=init_filter,
-    orientation="vertical"
-)
-
-rax = fig.add_axes([0.40, 0.82, 0.35, 0.12])
-check = CheckButtons(rax, ('Harmonic via noise', 'Harmonic filtered'), (False, False))
-
-# The function to be called anytime a slider's value changes
-def update(val):
+    def __init__(self):
+        self.t = np.linspace(0, 1, 1000)
+        # Define initial parameters
+        self.init_amplitude = 5
+        self.init_frequency = 3
+        self.init_phase = 0
+        self.init_noise_mean = 0
+        self.init_noise_covariance = 0
+        self.init_filter = 0.03
+        self.prev_mean = None
+        self.prev_covar = None
+        
     
-    b, a = signal.butter(4, flt_slider.val, btype='low')# , analog=False)
+    def check_noise_need(self, noise_mean, noise_covar):
+        if self.prev_mean == noise_mean and self.prev_covar == noise_covar: return False
+        self.prev_mean = noise_mean 
+        self.prev_covar = noise_covar
+        return True
+        
+    
+    # The parametrized function to be plotted
+    def f(self, t, amplitude, frequency, phase): # harmonic
+        return amplitude * np.sin(2 * np.pi * frequency * t + phase)
+    
+    def f1(self, t, amplitude, frequency, phase, noise_mean, noise_covariance): # harmonic_with_noise
+        if self.check_noise_need(noise_mean, noise_covariance):
+            self.noise = np.random.normal(noise_mean, noise_covariance, len(t))
+        # print(self.check_noise_need(noise_mean, noise_covariance))
+        return amplitude * np.sin(2 * np.pi * frequency * t + phase) + self.noise
+    
+    def create_figure(self):
+    # Create the figure and the line that we will manipulate
+        self.fig, (ax0, ax1) = plt.subplots(1, 2)
+        
+        
+        self.line2, = ax0.plot(self.t, self.f1(self.t, self.init_amplitude, self.init_frequency, self.init_phase, self.init_noise_mean, self.init_noise_covariance), visible=False, color='#FF5722', lw=2)
+        self.line, = ax0.plot(self.t, self.f(self.t, self.init_amplitude, self.init_frequency, self.init_phase), color='#3F51B5', lw=2)
+        
+        # b, a = signal.iirfilter(3, 0.5, btype='low')
+        b, a = signal.butter(4, self.init_filter, btype='low')#, analog=False)
+        
+        filtered_harmonic = signal.lfilter(b, a, self.f1(self.t, self.init_amplitude, self.init_frequency, self.init_phase, self.init_noise_mean, self.init_noise_covariance))
+        self.line3, = ax1.plot(self.t, filtered_harmonic, visible=False, lw=2)
+        
+        ax0.set_xlabel('Time [s]')
+        ax0.set_title('Harmonic')
+        ax1.set_title('Filtered Harmonic')
+        
+        # adjust the main plot to make room for the sliders
+        self.fig.subplots_adjust(left=0.25, bottom=0.35, top=0.75)
+        
+        # Make a horizontal oriented slider to control the amplitude
+        axamp = self.fig.add_axes([0.25, 0.2, 0.65, 0.03])
+        self.amp_slider = Slider(
+            ax=axamp,
+            label="Amplitude",
+            valmin=0,
+            valmax=10,
+            valinit=self.init_amplitude,
+            #orientation="vertical"
+            color='red'
+        )
+        
+        # Make a horizontal slider to control the frequency.
+        axfreq = self.fig.add_axes([0.25, 0.15, 0.65, 0.03])
+        self.freq_slider = Slider(
+            ax=axfreq,
+            label='Frequency [Hz]',
+            valmin=0.1,
+            valmax=30,
+            valinit=self.init_frequency,
+            color='grey'
+        )
+        
+        # Make a horizontal slider to control the phase.
+        axphas = self.fig.add_axes([0.25, 0.1, 0.65, 0.03])
+        self.phas_slider = Slider(
+            ax=axphas,
+            label='Phase',
+            valmin=0,
+            valmax=2*np.pi,
+            valinit=self.init_phase,
+            color='purple'
+        )
+        
+        # Make a vertically oriented slider to control the Noise mean
+        nmamp = self.fig.add_axes([0.15, 0.35, 0.0225, 0.53])
+        self.noismean_slider = Slider(
+            ax=nmamp,
+            label="Mean",
+            valmin=-5,
+            valmax=5,
+            valinit=self.init_noise_mean,
+            orientation="vertical",
+            color='#3F51B5'
+        )
+        
+        # Make a vertically oriented slider to control the Noise covariance
+        ncamp = self.fig.add_axes([0.08, 0.35, 0.0225, 0.53])
+        self.noiscovar_slider = Slider(
+            ax=ncamp,
+            label="Covar",
+            valmin=0,
+            valmax=5,
+            valinit=self.init_noise_covariance,
+            orientation="vertical",
+            color='#FF5722'
+        )
+        
+        fltamp = self.fig.add_axes([0.95, 0.35, 0.0225, 0.53])
+        self.flt_slider = Slider(
+            ax=fltamp,
+            label="Filter",
+            valmin=0.001,
+            valmax=0.1,
+            valinit=self.init_filter,
+            orientation="vertical"
+        )
+        
+        rax = self.fig.add_axes([0.40, 0.82, 0.35, 0.12])
+        self.check = CheckButtons(rax, ('Harmonic via noise', 'Harmonic filtered'), (False, False))
+        
+        resetax = self.fig.add_axes([0.8, 0.025, 0.1, 0.04])
+        self.button = Button(resetax, 'Reset', hovercolor='0.975')
+    
+    # The function to be called anytime a slider's value changes
+    def update(self, val):
+        
+        b, a = signal.butter(4, self.flt_slider.val, btype='low')# , analog=False)
+        
+        
+        
+        self.line2.set_ydata(self.f1(self.t, self.amp_slider.val, self.freq_slider.val, self.phas_slider.val, self.noismean_slider.val, self.noiscovar_slider.val))
+        self.line.set_ydata(self.f(self.t, self.amp_slider.val, self.freq_slider.val, self.phas_slider.val))
+        
+        filtered_harmonic = signal.lfilter(b, a, self.f1(self.t, self.amp_slider.val, self.freq_slider.val, self.phas_slider.val, self.noismean_slider.val, self.noiscovar_slider.val))
+        self.line3.set_ydata(filtered_harmonic)
+        
+        self.fig.canvas.draw_idle()
     
     
+    def func(self, label):
+        if label == 'Harmonic via noise':
+            # line.set_visible(not line.get_visible())
+            self.line2.set_visible(not self.line2.get_visible())
+        elif label == 'Harmonic filtered':
+            self.line3.set_visible(not self.line3.get_visible())
+        plt.draw()
     
-    line2.set_ydata(f1(t, amp_slider.val, freq_slider.val, phas_slider.val, noismean_slider.val, noiscovar_slider.val))
-    line.set_ydata(f(t, amp_slider.val, freq_slider.val, phas_slider.val))
     
-    filtered_harmonic = signal.lfilter(b, a, f1(t, amp_slider.val, freq_slider.val, phas_slider.val, noismean_slider.val, noiscovar_slider.val))
-    line3.set_ydata(filtered_harmonic)
+    # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
+    def reset(self, event):
+        self.freq_slider.reset()
+        self.amp_slider.reset()
+        self.phas_slider.reset()
+        self.noismean_slider.reset()
+        self.noiscovar_slider.reset()
+        self.flt_slider.reset()
     
-    fig.canvas.draw_idle()
+    # print(line2.get_ydata().size
+    
+    # Корисні посилання
+    # https://matplotlib.org/stable/gallery/widgets/index.html
+    # https://docs.scipy.org/doc/scipy/reference/signal.html
+    # https://plotly.com/python/#controls
+    # https://docs.bokeh.org/en/latest/docs/user_guide/interaction/widgets.html
+    # https://altair-viz.github.io/user_guide/interactions.html
 
-
-def func(label):
-    if label == 'Harmonic via noise':
-        # line.set_visible(not line.get_visible())
-        line2.set_visible(not line2.get_visible())
-    elif label == 'Harmonic filtered':
-        line3.set_visible(not line3.get_visible())
-    plt.draw()
+matplotapp = MPLTTask()
+matplotapp.create_figure()
 
 # register the update function with each slider
-check.on_clicked(func)
-freq_slider.on_changed(update)
-amp_slider.on_changed(update)
-phas_slider.on_changed(update)
-noismean_slider.on_changed(update)
-noiscovar_slider.on_changed(update)
-flt_slider.on_changed(update)
-
-# Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-resetax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-button = Button(resetax, 'Reset', hovercolor='0.975')
-
-
-def reset(event):
-    freq_slider.reset()
-    amp_slider.reset()
-    phas_slider.reset()
-    noismean_slider.reset()
-    noiscovar_slider.reset()
-    flt_slider.reset()
-button.on_clicked(reset)
+matplotapp.check.on_clicked(matplotapp.func)
+matplotapp.freq_slider.on_changed(matplotapp.update)
+matplotapp.amp_slider.on_changed(matplotapp.update)
+matplotapp.phas_slider.on_changed(matplotapp.update)
+matplotapp.noismean_slider.on_changed(matplotapp.update)
+matplotapp.noiscovar_slider.on_changed(matplotapp.update)
+matplotapp.flt_slider.on_changed(matplotapp.update)
+matplotapp.button.on_clicked(matplotapp.reset)
 
 plt.show()
-
-# print(line2.get_ydata().size
-
-# Корисні посилання
-# https://matplotlib.org/stable/gallery/widgets/index.html
-# https://docs.scipy.org/doc/scipy/reference/signal.html
-# https://plotly.com/python/#controls
-# https://docs.bokeh.org/en/latest/docs/user_guide/interaction/widgets.html
-# https://altair-viz.github.io/user_guide/interactions.html
